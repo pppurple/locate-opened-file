ae9d28d706c9cb09a1933fc7f21daef67cccbace
pppurple <shuggie.otis0@gmail.com>
2021-03-13 04:35:55 +0900

    add initial skeleton project

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("org.jetbrains.intellij") version "0.7.2"
+    kotlin("jvm") version "1.4.31"
+}
+
+group = "org.example.pppurple"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version = "2020.3.2"
+}
+tasks.getByName<org.jetbrains.intellij.tasks.PatchPluginXmlTask>("patchPluginXml") {
+    changeNotes("""
+      Add change notes here.<br>
+      <em>most HTML tags may be used</em>""")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "manually-select-opened-file"
+
